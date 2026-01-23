7be6d873135dda2fea211d16f434336d91520097
Mamyrbay-Zhuldyz <z_mamyrbai@kbtu.kz>
2026-01-23 15:17:58 +0600

    Add casting.py and strings.py

---
+++ Practice1/python-basics/strings.py
@@ -0,0 +1,17 @@
+#Example 1
+print("It's alright")
+print("He is called 'Johnny'")
+print('He is called "Johnny"')
+
+
+#Example 2
+a = "Hello"
+print(a)
+
+
+#Example 3
+a = """Lorem ipsum dolor sit amet,
+consectetur adipiscing elit,
+sed do eiusmod tempor incididunt
+ut labore et dolore magna aliqua."""
+print(a)
