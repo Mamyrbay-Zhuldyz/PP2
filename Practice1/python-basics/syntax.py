2ebacf1d72cda9d3339e15b33335be822cb88fea
Mamyrbay-Zhuldyz <z_mamyrbai@kbtu.kz>
2026-01-23 13:28:26 +0600

    Add hello_world, syntax and comments

---
+++ Practice1/python-basics/syntax.py
@@ -0,0 +1,11 @@
+# 1. Correct indents
+print("Correct indents")
+if 5 > 2:
+    print("Five is greater than two!") # <- 4 spaces or Tab
+    print("This is also inside if")    # <- equal indentation
+
+
+# 2. Incorrect indentation (error!)
+print("\nExample 2: Indentation error (commented out)")
+# if 5 > 2:
+# print("This will cause an error")  # SyntaxError!
