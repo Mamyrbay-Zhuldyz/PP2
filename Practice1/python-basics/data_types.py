99559e438f8c4028ef891eeba487fffdfcb6f2f4
Mamyrbay-Zhuldyz <z_mamyrbai@kbtu.kz>
2026-01-23 15:00:55 +0600

    Add variables.py, data_types.py, numbers.py

---
+++ Practice1/python-basics/data_types.py
@@ -0,0 +1,29 @@
+#Example 1
+x = 20
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
+
+
+
+#Example 2
+x = ["apple", "banana", "cherry"]
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x))
+
+
+#Example 3
+x = None
+
+#display x:
+print(x)
+
+#display the data type of x:
+print(type(x)) 
